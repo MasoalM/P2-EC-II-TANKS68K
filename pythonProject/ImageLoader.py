c17d7b9b1233415f49debe4a62f523be61de43b5
Sergilanovas22 <133489467+Sergilanovas22@users.noreply.github.com>
2024-12-15 12:37:26 +0100

    erratón
    erratón

--- pythonProject/ImageLoader.py
+++ pythonProject/ImageLoader.py
@@ -1,13 +1,24 @@
-from skimage.io import imread
+from PIL import Image
+import numpy as np
 
-theImage = imread('../IMAGES/tankIntro.jpg')
-numRows, numCols, _ = theImage.shape
-outData = []
+# Cargar la imagen
+ruta_imagen = "D:/CARRERA/Tercer año - Curso 24_25/download.jpeg"
+imagen = Image.open(ruta_imagen).convert("RGB")  # Convertir a RGB si no lo está
 
-for r in range(numRows):
-    for c in range(numCols):
-        theRed, theGreen, theBlue = theImage[r, c]
-        outData += [0, theBlue, theGreen, theRed]
+# Convertir la imagen a un array NumPy
+array_rgb = np.array(imagen)
 
-with open('SALIDA.bin', 'wb') as outFile:
-    outFile.write(bytearray(outData))
+# Cambiar de RGB a BGR
+array_bgr = array_rgb[:, :, ::-1]
+
+# Convertir a hexadecimal y agregar los dos ceros delante
+# Ejemplo: Para un píxel [255, 0, 128] en BGR => "00FF0080"
+array_hex = np.apply_along_axis(
+    lambda pixel: f"00{pixel[0]:02X}{pixel[1]:02X}{pixel[2]:02X}", axis=2, arr=array_bgr
+)
+
+# Guardar en un archivo de texto
+ruta_txt = "salida3.txt"
+with open(ruta_txt, "w") as archivo:
+    for fila in array_hex:
+        archivo.write("".join(fila))
