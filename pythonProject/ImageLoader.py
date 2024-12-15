038a5183e12899e013c0751b3da94993d9dbe0fe
Marcos Socías <105206059+MasoalM@users.noreply.github.com>
2024-12-15 23:12:08 +0100

    V9.5
    Tanques se pintan pero va lento (2 tanques en pantalla son 31 fps, 3 son 24 fps, 5 son 15 fps)

--- pythonProject/ImageLoader.py
+++ pythonProject/ImageLoader.py
@@ -1,6 +1,6 @@
 from skimage.io import imread
 
-theImage = imread('../IMAGES/PLAYER.jpg')
+theImage = imread('../IMAGES/ENEMY.jpg')
 numRows, numCols, _ = theImage.shape
 outData = []
 
@@ -9,5 +9,5 @@ for r in range(numRows):
         theRed, theGreen, theBlue = theImage[r, c]
         outData += [0, theBlue, theGreen, theRed]
 
-with open('PLAYER.bin', 'wb') as outFile:
+with open('ENEMY.bin', 'wb') as outFile:
     outFile.write(bytearray(outData))
