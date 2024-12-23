4c4fe0fa298e11c83790ddba986ba7f6e9087f66
Marcos Socías <105206059+MasoalM@users.noreply.github.com>
2024-12-23 13:07:59 +0100

    V C.2
    Colores de fondo agregados e imagen redimensionada para que no tarde tanto en cargar el juego

--- pythonProject/ImageLoader.py
+++ pythonProject/ImageLoader.py
@@ -1,6 +1,6 @@
 from skimage.io import imread
 
-theImage = imread('../IMAGES/PLAYER/PLAYER_Y_W.jpg')
+theImage = imread('../IMAGES/tanksIntro.jpg')
 numRows, numCols, _ = theImage.shape
 outData = []
 
@@ -9,5 +9,5 @@ for r in range(numRows):
         theRed, theGreen, theBlue = theImage[r, c]
         outData += [0, theBlue, theGreen, theRed]
 
-with open('OUTPUT/PLAYER_OUTPUT/PLAYER_Y_W.bin', 'wb') as outFile:
+with open('OUTPUT/MENUIMAGE.bin', 'wb') as outFile:
     outFile.write(bytearray(outData))
